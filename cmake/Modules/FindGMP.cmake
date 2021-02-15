8f7a702df4a9926b84bc78ad4b804aced26b1d5e
Mezhir <mezhir@Inspiron.vz-wi.no-ip.biz>
2021-02-14 18:20:07 -0600

    Refactor FindGMP and use GMP::GMP target.

--- cmake/Modules/FindGMP.cmake
+++ cmake/Modules/FindGMP.cmake
@@ -8,7 +8,10 @@ set(USE_SYSTEM_GMP ${ENABLE_SYSTEM_GMP})
 if(ENABLE_SYSTEM_GMP)
 	find_path(GMP_INCLUDE_DIR NAMES gmp.h)
 	find_library(GMP_LIBRARY NAMES gmp)
-	if(NOT GMP_INCLUDE_DIR OR NOT GMP_LIBRARY)
+	if(GMP_INCLUDE_DIR AND GMP_LIBRARY)
+		message(STATUS "Using GMP provided by system.")
+		mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARY)
+	else()
 		message (STATUS "Detecting GMP from system failed.")
 		set(USE_SYSTEM_GMP FALSE)
 	endif()
@@ -17,23 +20,8 @@ else()
 		"Detecting GMP from system disabled! (ENABLE_SYSTEM_GMP=0)")
 endif()
 
-if(NOT USE_SYSTEM_GMP)
-	# Failed to find system GMP, so set vars for bundled mini-gmp.
-	set(GMP_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/lib/gmp"
-		CACHE INTERNAL "GMP include directory.")
-	set(GMP_LIBRARY "bundled mini-gmp" CACHE INTERNAL "GMP library.")
-endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP
-	REQUIRED_VARS GMP_INCLUDE_DIR GMP_LIBRARY
-)
-
-mark_as_advanced(GMP_FOUND GMP_INCLUDE_DIR GMP_LIBRARY)
-
-if(ENABLE_SYSTEM_GMP AND GMP_FOUND)
+if(USE_SYSTEM_GMP)
 	# GMP library has been found in system. Make imported target.
-	message(STATUS "Using GMP provided by system.")
 	add_library(GMP::GMP INTERFACE IMPORTED)
 	target_include_directories(GMP::GMP INTERFACE "${GMP_INCLUDE_DIR}")
 	target_link_libraries(GMP::GMP INTERFACE "${GMP_LIBRARY}")
